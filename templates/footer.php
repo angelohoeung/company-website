244ce755df764c49e22f3b29e2e0967dad470324
Angelo Hoeung <hi@hoeung.ca>
2024-12-08 22:08:09 -0500

    change copyright

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
     </main>
     <footer class="footer footer-center bg-base-200 text-base-content p-4 mt-4 md:mt-0">
         <aside>
-            <p>Copyright &copy; 2024 - Group 7 (Angelo Hoeung, Adi Naeem, Huzaifah Ahmad)</p>
+            <p>Copyright &copy; 2024 - Angelo Hoeung</p>
         </aside>
     </footer>
 </body>
